d691b524e5f267a7ab6c37303b3b9bed13ae3d3f
Ben Wencke <benwencke@gmail.com>
2016-03-01 20:48:22 -0500

    made paths relative

--- src/init.sh
+++ src/init.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-export PATH=$PATH:/Volumes/case-sensitive/esp-open-sdk/xtensa-lx106-elf/bin/
+export PATH=$PATH:../xtensa-lx106-elf/bin/
 make clean
-make SDK_BASE="/Volumes/case-sensitive/esp-open-sdk/esp_iot_sdk_v1.5.2" FLAVOR="release" all
+make SDK_BASE="../esp_iot_sdk_v1.5.2" FLAVOR="release" all
---
+++ xtensa-lx106-elf/lib/gcc/xtensa-lx106-elf/4.8.2/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "/Volumes/case-sensitive/esp-open-sdk/crosstool-NG/.build/src/gcc-4.8.2/configure --build=x86_64-build_apple-darwin15.0.0 --host=x86_64-build_apple-darwin15.0.0 --target=xtensa-lx106-elf --prefix=/Volumes/case-sensitive/esp-open-sdk/xtensa-lx106-elf --with-local-prefix=/Volumes/case-sensitive/esp-open-sdk/xtensa-lx106-elf/xtensa-lx106-elf/sysroot --disable-libmudflap --with-sysroot=/Volumes/case-sensitive/esp-open-sdk/xtensa-lx106-elf/xtensa-lx106-elf/sysroot --with-newlib --enable-threads=no --disable-shared --with-pkgversion='crosstool-NG 1.20.0' --disable-__cxa_atexit --with-gmp=/Volumes/case-sensitive/esp-open-sdk/crosstool-NG/.build/xtensa-lx106-elf/buildtools --with-mpfr=/Volumes/case-sensitive/esp-open-sdk/crosstool-NG/.build/xtensa-lx106-elf/buildtools --with-mpc=/Volumes/case-sensitive/esp-open-sdk/crosstool-NG/.build/xtensa-lx106-elf/buildtools --with-isl=/Volumes/case-sensitive/esp-open-sdk/crosstool-NG/.build/xtensa-lx106-elf/buildtools --with-cloog=/Volumes/case-sensitive/esp-open-sdk/crosstool-NG/.build/xtensa-lx106-elf/buildtools --with-libelf=/Volumes/case-sensitive/esp-open-sdk/crosstool-NG/.build/xtensa-lx106-elf/buildtools --enable-lto --enable-target-optspace --disable-libgomp --disable-libmudflap --disable-nls --disable-multilib --enable-languages=c,c++";
+static const char thread_model[] = "single";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { NULL, NULL} };
